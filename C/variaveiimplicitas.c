b87e6f867abc25f43ccb993d0402cc3a88cc3ba1
Rubens Pablo Suassuna Avelino <91734025+rubenspablo@users.noreply.github.com>
2025-09-03 18:36:12 +0000

    salvando

---
+++ C/variaveiimplicitas.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    int a = 10;
+    float b = 3.5;
+    float resultado = a + b; // 'a' é implicitamente convertido para float 
+    printf("Resultado: %.2f\n", resultado);
+    return 0;
+}
---
+++ C/variaveisdepontoflutuant.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    float x = 5.5;
+    float y = 2.2;
+    float soma = x + y;
+    float diferenca = x - y;
+    float produto = x * y; 
+    float quociente = x / y; // A divisão de floats resulta em um float
+    printf("Soma: %.2f\n", soma);
+    printf("Diferença: %.2f\n", diferenca);
+    printf("Produto: %.2f\n", produto);
+    printf("Quociente: %.2f\n", quociente);
+    return 0;
+}
