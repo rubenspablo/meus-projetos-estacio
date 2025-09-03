508371088fd7179ee620f5361a683703b53247e0
Rubens Pablo Suassuna Avelino <91734025+rubenspablo@users.noreply.github.com>
2025-09-03 21:57:48 +0000

    salva

---
+++ C/modificadoresdetiposdeunsign.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int numeroSinal = 3000000000;
+    unsigned int numeroSemSinal = 3000000000;
+    printf("Numero com sinal: %d\n", numeroSinal);
+    printf("Numero sem sinal: %u\n", numeroSemSinal);0,
+
+
+
+
+}
