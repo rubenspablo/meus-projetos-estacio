e5298f980651680eb88151b67b3f681d285b469a
Rubens Pablo Suassuna Avelino <91734025+rubenspablo@users.noreply.github.com>
2025-09-03 17:27:19 +0000

    salva

--- C/incrementodecrimento.c
+++ C/incrementodecrimento.c
@@ -11,10 +11,17 @@ int main() {
   Pós-Decremento (variavel--)
   */
     int numero = 1;
+    int resultado;
+    resultado = numero++;
     printf("Antes do incremento: %d\n", numero);
     numero++; // numero = numero + 1
     printf("Após incremento: %d\n", numero);
     numero--;
-    printf("Após decremento: %d\n", numero);
+    printf("Após decremento: %d\n\n", numero);
+    resultado = ++numero; // Pré-incremento
+    printf("Resultado do pré-incremento: %d\n", resultado);
+    resultado = numero++; // Pós-incremento
+    printf("Resultado do pós-incremento: %d\n", resultado);
+
   return 0;
 }
