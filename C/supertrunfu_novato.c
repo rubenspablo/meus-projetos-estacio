dd04b339fb0ecbd846a4cf875cbbb3022b8f6038
Rubens Pablo Suassuna Avelino <91734025+rubenspablo@users.noreply.github.com>
2025-09-03 11:20:30 +0000

    salvando

--- C/supertrunfu_novato.c
+++ C/supertrunfu_novato.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 int main(){
     // as variaveis foram declaradas juntas das duas cartas
-    char estado1, estado2;
+    char estado1[40], estado2[40];
     char codigo1[10], codigo2[10];
     char cidade1[50], cidade2[50];
+    char letra1, letra2;
     int populacao1, populacao2;
     float area1, area2;
     float pib1, pib2;
@@ -16,18 +17,23 @@ int main(){
     printf("Você terá direito a duas cartas inicialmente.\n");
     printf("Então seja inteligente nas suas escolhas e boa sorte.\n\n");
 
-    printf("Vamos começar com a primeira carta.\n");
-    printf("Escolha seu estado indicando uma letra para ele entre A - H\n");
-    scanf(" %c", &estado1);
-    printf("A Letra escolhida por você foi %c \n", estado1);
+    printf("Vamos começar com a primeira carta.\n\n");
+    printf("Escolha seu estado.\n");
+    scanf(" %[^\n]", estado1);
+
+    printf("Escolha uma letra indicando o inicio do codigo da sua carta entre A - H\n");
+    scanf(" %c", &letra1);
+    printf("A Letra escolhida por você foi %c \n", letra1);
+
     printf("Agora vamos digitar um codigo para juntar com a letra que você ja escolheu\n");
     printf("Então vamos lá digite o codigo, os números seram de 01 à 04\n");
     scanf("%s", codigo1);
+
     printf("Sua carta tem o codigo = %c%s\n", estado1, codigo1);
     
     printf("Agora vamos preencher os dados da sua carta\n");
     printf("Qual o nome da sua cidade?\n");
-    scanf("%s", cidade1);
+    scanf(" %[^\n]", cidade1);
     printf("Qual a sua população?\n");
     scanf("%d", &populacao1);
 
@@ -43,17 +49,24 @@ int main(){
     // Carta 02
     printf("Vamos começar com a segunda carta.\n");
     printf("Lembre-se seja inteligente nas escolhas\n");
-    printf(" Escolha seu estado indicando uma letra para ele entre A - H\n");
-    scanf(" %c", &estado2);
-    printf("A Letra escolhida por você foi %c\n", estado2);
+
+    printf("Vamos para a segunda carta.\n");
+    printf("Escolha seu estado.\n");
+    scanf(" %[^\n]", estado2);
+
+    printf("Escolha uma letra indicando o inicio do codigo da sua carta entre A - H\n");
+    scanf(" %c", &letra2);
+    printf("A Letra escolhida por você foi %c\n", letra2);
+
     printf("Agora vamos digitar um codigo para juntar com a letra que você ja escolheu\n");
     printf("Então vamos lá digite o codigo, os números seram de 01 à 04\n");
     scanf("%s", codigo2);
-    printf("Sua carta tem o codigo = %c%s\n", estado2, codigo2);
+
+    printf("Sua carta tem o codigo = %c%s\n", letra2, codigo2);
 
     printf("Agora vamos preencher os dados da sua carta\n");
     printf("Qual o nome da sua cidade?\n");
-    scanf("%s", cidade2);
+    scanf(" %[^\n]", cidade2);
 
     printf("Qual a sua população?\n");
     scanf("%d", &populacao2);
