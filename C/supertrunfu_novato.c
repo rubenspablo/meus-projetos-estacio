76301c8817f8ef87647bc3d68e9fedfd735e2985
Rubens Pablo Suassuna Avelino <91734025+rubenspablo@users.noreply.github.com>
2025-09-03 11:32:35 +0000

    salvando

--- C/supertrunfu_novato.c
+++ C/supertrunfu_novato.c
@@ -82,22 +82,22 @@ int main(){
 
     printf("Agora vamos ver suas cartas\n\n");
 
-    printf("Carta 01");
+    printf("Carta 01\n");
     printf("Estado: %s\n", estado1);
     printf("Código: %c%s\n", letra1, codigo1);
     printf("Cidade: %s\n", cidade1);
-    printf("População: %d\n", populacao1);
-    printf("Área: %.2f\n", area1);
-    printf("PIB: %.2f\n", pib1);
+    printf("População: %d de habitantes.\n", populacao1);
+    printf("Área: %.2f Km²\n", area1);
+    printf("PIB: %.2f bilhões de Reais\n", pib1);
     printf("Pontos turísticos: %d\n\n", pontos1);
 
-    printf("Carta 02");
+    printf("Carta 02\n");
     printf("Estado: %s\n", estado2);
     printf("Código: %c%s\n", letra2, codigo2);
     printf("Cidade: %s\n", cidade2);
-    printf("População: %d\n", populacao2);
-    printf("Área: %.2f\n", area2);
-    printf("PIB: %.2f\n", pib2);
+    printf("População: %d de habitantes.\n", populacao2);
+    printf("Área: %.2f Km²\n", area2);
+    printf("PIB: %.2f bilhões de Reais\n", pib2);
     printf("Pontos turísticos: %d\n\n", pontos2);
 
     printf("Agora você conhece suas cartas e está preparado para o novo mundo\n");
