6f402f91942a833fb80546862c5cb9f9a5634c79
Rubens Pablo Suassuna Avelino <91734025+rubenspablo@users.noreply.github.com>
2025-09-14 09:56:21 +0000

    salvando alterações em C/numerosgrandes.c

---
+++ C/funcoes.c
@@ -0,0 +1,29 @@
+    #include <stdio.h>
+     
+
+    // Função genérica para entrada de dados.
+    void entradadeDados(){
+        //codigo para função entrada de dados
+    }
+
+
+
+    // Função genérica para cálculo de média.
+    float calculoMedia(){
+
+        // codigo para função cálculo da média
+    }
+
+
+
+    // Função genérica para exibir dados.
+    void exibicaoDados(){
+        //codigo para função exibição de dados
+    }
+
+    int main() {
+        entradadeDados();
+        float media = calculoMedia();
+        exibicaoDados();        
+        return 0;
+    }
---
+++ C/numerosgrandes.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    unsigned long int numeroGrandePsositivo =4000000000;
+    unsigned int numeroPositivo = 4000000000;
+    long int    numeroGrande = 4000000000; 
+    int numero = 4000000000;
+
+
+    printf("Numero positivo grande: %ld\n", numeroGrandePsositivo);
+    printf("Numero Positivo: %u\n", numeroPositivo);                   
+    printf("Numero grande : %ld\n", numeroGrande);
+    printf("Numero : %d\n", numero);
+    return 0;
+
+
+
+}
