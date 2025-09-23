e6e374597d7e5bee81c79eba8248074037743898
Rubens Pablo Suassuna Avelino <91734025+rubenspablo@users.noreply.github.com>
2025-09-23 13:43:29 +0000

    sanvando

--- C/Nivel 02/notas.c
+++ C/Nivel 02/notas.c
@@ -16,13 +16,17 @@
 
     if (nota >= 90) {
         printf("Você tirou A!\n");
-    } else if (nota >= 80) {
+    } else 
+        if (nota >= 80) {
         printf("Você tirou B!\n");
-    } else if (nota >= 70) {
+    } else 
+        if (nota >= 70) {
         printf("Você tirou C!\n");
-    } else if (nota >= 60) {
+    } else 
+        if (nota >= 60) {
         printf("Você tirou D!\n");
-    } else if (nota >= 50) {
+    } else 
+        if (nota >= 50) {
         printf("Você tirou E!\n");
     } else {
         printf("Você tirou F!\n");
