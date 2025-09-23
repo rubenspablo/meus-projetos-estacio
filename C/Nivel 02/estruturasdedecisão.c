5966a7593a424c0136275ce349a3ec097edfa311
Rubens Pablo Suassuna Avelino <91734025+rubenspablo@users.noreply.github.com>
2025-09-23 11:51:53 +0000

    sanvando

---
+++ C/Nivel 02/estruturasdedecisão.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+       int main(){
+
+        int Laranja = 76.0
+        int Abacaxi = 100.0
+
+         is (laranja == abacaxi) {
+            printf("Os preços são iguais.\n");
+        } else {
+            printf("Os preços são diferentes.\n");
+
+
+
+       }
+    }
